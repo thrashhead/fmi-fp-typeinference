814c76a743cb1cd6fc702910000a76d387ec6f47
Georgi Lyubenov <godzbanebane@gmail.com>
2022-02-20 20:31:53 +0200

    Add notes

--- src/Types.hs
+++ src/Types.hs
@@ -1,15 +1,16 @@
 {-# LANGUAGE InstanceSigs #-}
+
 module Types where
 
-import Helper ( joinDistinctLeft )
+import Helper (joinDistinctLeft)
 
+-- use a newtype
 type VarName = String
 
-data Type = 
-    TypeVar VarName |
-    TypeFunc Type Type
+data Type
+  = TypeVar VarName
+  | TypeFunc Type Type
   deriving (Eq, Ord, Show)
-
 
 typeOf :: Polytype -> Type
 typeOf (Dot ty) = ty
@@ -17,10 +18,10 @@ typeOf (Forall _ rest) = typeOf rest
 
 varsOf :: Polytype -> [VarName]
 varsOf pt = joinDistinctLeft boundVars typeVars
-    where
-        boundVars = boundVarsOf pt
-        typeVars = typeVarsOf $ typeOf pt
-        
+  where
+    boundVars = boundVarsOf pt
+    typeVars = typeVarsOf $ typeOf pt
+
 typeVarsOf :: Type -> [VarName]
 typeVarsOf (TypeVar var) = [var]
 typeVarsOf (TypeFunc t1 t2) = joinDistinctLeft (typeVarsOf t1) (typeVarsOf t2)
@@ -29,19 +30,41 @@ boundVarsOf :: Polytype -> [VarName]
 boundVarsOf (Dot _) = []
 boundVarsOf (Forall bound rest) = bound : boundVarsOf rest
 
-data Exp =
-    ExpVar VarName |
-    ExpApp Exp Exp |
-    ExpLam VarName Exp
+data Exp
+  = ExpVar VarName
+  | ExpApp Exp Exp
+  | ExpLam VarName Exp
   deriving (Eq, Ord, Show)
 
-data Polytype = 
-    Forall VarName Polytype |
-    Dot Type 
+-- use a nameless representation
+-- безименно представяне
+data Polytype
+  = Forall VarName Polytype
+  | Dot Type
   deriving (Eq, Show)
+
+-- Forall "x" (Dot (TypeVar "x")) == Forall "y" (Dot (TypeVar "y"))
+
+-- \x. \y. x
+-- \.\.1
+-- \x. \y. z
+-- \.\. 2
+
+-- \forall x. x
+-- \forall y. y
+-- \forall. 0 == \forall. 0
+
+-- \forall x. x ==
+-- \forall. 0
+
+-- \forall x. \forall y. x
+-- \forall. \forall. 1
+
+-- \forall x. y
+
+-- \forall x. y
+-- y -> x
+-- \forall x. x
 
 polytypeFrom :: [VarName] -> Type -> Polytype
 polytypeFrom xs ty = foldr Forall (Dot ty) xs
-
-
-
